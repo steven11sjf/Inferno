9b98f791e221e5303c90076703db5281614353f2
Steven Franklin <steve11sjf@gmail.com>
2020-07-09 00:09:49 -0500

    Basic movement and shooting
    - Updated .gitignore to include the Library/ folder
    - Added full WASD movement and a ms paint stick figure animations
    - Set the background to orange
    - Added a green crosshair at mouse coordinates
    - Added a bullet prefab
    - When M1 is pressed a new bullet prefab is created and travels from
    Daniel to crosshair's position
    - Bullets travel at 5.0f regardless of crosshair distance

--- Inferno/Assets/Scripts/BasicMovement.cs
+++ Inferno/Assets/Scripts/BasicMovement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-
+    // used to change animator state
     public Animator animator;
+
+    public GameObject crosshair; // the crosshair's location
+    public GameObject bulletPrefab; // prefab for bullets
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +19,46 @@ public class BasicMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        // get & set horizontal and vertical movement
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        MoveCrosshair();
 
+        // shoot if this is the first frame M1 was pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
+        // set animator variables
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        // update Daniel's position
+        transform.position = transform.position + movement * Time.deltaTime;
+
+    }
+
+    private void MoveCrosshair()
+    {
+        // get point on screen where mouse is
+        Vector3 aim = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+        aim = Camera.main.ScreenToWorldPoint(aim);
+        Vector3 followXOnly = new Vector3(aim.x, aim.y, transform.position.z);
+        crosshair.transform.position = followXOnly;
+    }
+
+    private void Shoot()
+    {
+        // get a normalized vector pointing from Daniel to the crosshair
+        Vector3 danielToCrosshair = crosshair.transform.position - transform.position;
+        danielToCrosshair.Normalize();
+
+        // instantiate a new bullet from the center of daniel
+        GameObject bullet = Instantiate(bulletPrefab, transform.position + new Vector3(0.15f, -0.15f, 0.0f), Quaternion.identity);
+
+        // fire bullet in the direction of the crosshair
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(danielToCrosshair.x, danielToCrosshair.y) * 5.0f;
     }
 }
