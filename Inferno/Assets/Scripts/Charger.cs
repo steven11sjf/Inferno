e1f33fac366a43dd6b4a5d8ca60d07af16b01353
Steven Franklin <steve11sjf@gmail.com>
2020-07-13 20:53:53 -0500

    Added walls and implemented Unity's physics
    - player and enemy now move using velocity in a rigidbody
    - added three types of wall (horizontal, vertical and tower)
    - implemented collision boxes for each
    - charger now has animations for idle, wind-up, charging, and stunned
    - charger can "slide" along the wall if the angle is lower than 30
    degrees
    - reworked stun so it only affects after charger hits player or wall
    - if charger is dodged or uses his entire charge he does not get stunned
    - charge stun lasts 5 seconds
    - increased speed and turn radius on charger to account for new
    behaviors

--- Inferno/Assets/Scripts/Charger.cs
+++ Inferno/Assets/Scripts/Charger.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Charger : MonoBehaviour
 {
 
-    public GameObject player;
+    public GameObject player; // reference to player
+    public Rigidbody2D rb; // the rigidbody to access the physics engine
+    public Animator animator; // the animator
 
     public float CHARGE_SPEED; // how fast the charge goes
     public float CHARGE_TIME; // how long the charge will naturally go
@@ -13,6 +15,8 @@ public class Charger : MonoBehaviour
     public float CHARGE_STUN; // the amount of time stunned when you miss
     public float CHARGE_TURN_RADIUS; // the turn radius of the charge 
     public float CHARGE_OVERCHARGE_TIME; // how long the charger continues to charge after missing
+    public float REBOUND_SPEED; // the speed the charger bounces back after a collision
+    public float COLLISION_SLIDE_ANGLE; // the angle that allows the charger to "slide" along the wall instead of stopping
 
     private int charging;
     private float chargeTimer;
@@ -22,29 +26,26 @@ public class Charger : MonoBehaviour
     void Start()
     {
         charging = 0;
+        animator.SetInteger("ChargerState", 0);
         chargeTimer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(charging == 0) // not charging
+        if(charging == 0 && CanSeePlayer()) // not charging
         {
-            // raycast to check if player is in line of sight
-            RaycastHit2D[] hits = Physics2D.LinecastAll(transform.position, player.transform.position);
-
-            foreach (RaycastHit2D hit in hits)
+            if (CanSeePlayer())
             {
-                GameObject other = hit.collider.gameObject;
-                if (other.CompareTag("Walls")) break; // walls break los
-                if(other.CompareTag("Player"))
-                {
-                    // start windup and set charge time
-                    charging = 1;
-                    chargeTimer = Time.time + CHARGE_WINDUP;
-                    break;
-                }
+                // get direction of charge
+                chargeDir = player.transform.position - transform.position;
+                chargeDir.Normalize();
+
+                // start windup and set charge time
+                charging = 1;
+                animator.SetInteger("ChargerState", 1);
+                chargeTimer = Time.time + CHARGE_WINDUP;
+                return;
             }
         }
         else if(charging == 1) // winding up
@@ -52,12 +53,10 @@ public class Charger : MonoBehaviour
             // check timer
             if(Time.time > chargeTimer)
             {
-                // get direction of charge
-                chargeDir = player.transform.position - transform.position;
-                chargeDir.Normalize();
 
                 // start the charge and set the timer
                 charging = 2;
+                animator.SetInteger("ChargerState", 2);
                 chargeTimer = Time.time + CHARGE_TIME;
             }
         }
@@ -68,6 +67,7 @@ public class Charger : MonoBehaviour
             {
                 // end the charge
                 charging = 0;
+                animator.SetInteger("ChargerState", 0);
             }
 
             // calculate current direction
@@ -78,40 +78,98 @@ public class Charger : MonoBehaviour
             if (Vector3.Dot(chargeDir, curDir) < 0.5f)
             {
                 charging = 3; // overcharge
+                animator.SetInteger("ChargerState", 3);
                 chargeTimer = Time.time + CHARGE_OVERCHARGE_TIME;
-                transform.position = transform.position + chargeDir * Time.deltaTime * CHARGE_SPEED;
+                //transform.position = transform.position + chargeDir * Time.deltaTime * CHARGE_SPEED;
+                rb.velocity = new Vector2(chargeDir.x, chargeDir.y) * CHARGE_SPEED;
             }
 
-            // calculate charge (dot) (current rotated 90 deg counterclockwise)
-            float dot = chargeDir.y * curDir.x - chargeDir.x * curDir.y;
-            
-            if(dot > 0) // need rightwards adjustment
+            // if we have LOS to the player than adjust course, otherwise keep going straight
+            if (CanSeePlayer())
             {
-                chargeDir = Quaternion.Euler(0.0f, 0.0f, -CHARGE_TURN_RADIUS * Time.deltaTime) * chargeDir;
-            }
-            else if(dot < 0) // need leftwards adjustment
-            {
-                chargeDir = Quaternion.Euler(0.0f, 0.0f, CHARGE_TURN_RADIUS * Time.deltaTime) * chargeDir;
+                // calculate charge (dot) (current rotated 90 deg counterclockwise)
+                float dot = chargeDir.y * curDir.x - chargeDir.x * curDir.y;
+
+                if (dot > 0) // need rightwards adjustment
+                {
+                    chargeDir = Quaternion.Euler(0.0f, 0.0f, -CHARGE_TURN_RADIUS * Time.deltaTime) * chargeDir;
+                }
+                else if (dot < 0) // need leftwards adjustment
+                {
+                    chargeDir = Quaternion.Euler(0.0f, 0.0f, CHARGE_TURN_RADIUS * Time.deltaTime) * chargeDir;
+                }
             }
 
-            transform.position = transform.position + chargeDir * Time.deltaTime * CHARGE_SPEED;
+            rb.velocity = new Vector2(chargeDir.x, chargeDir.y) * CHARGE_SPEED;
+            //transform.position = transform.position + chargeDir * Time.deltaTime * CHARGE_SPEED;
         }
         else if(charging == 3) // overcharge
         {
             if(chargeTimer < Time.time)
             {
                 charging = 4;
+                animator.SetInteger("ChargerState", 4);
                 chargeTimer = Time.time + CHARGE_STUN;
             }
 
-            transform.position = transform.position + chargeDir * Time.deltaTime * CHARGE_SPEED;
+            rb.velocity = new Vector2(chargeDir.x, chargeDir.y) * CHARGE_SPEED;
+            //transform.position = transform.position + chargeDir * Time.deltaTime * CHARGE_SPEED;
         }
         else if(charging == 4) // stun
         {
             if(chargeTimer < Time.time)
             {
                 charging = 0;
+                animator.SetInteger("ChargerState", 0);
             }
+        }
+    }
+
+    // check if it has LOS to player
+    bool CanSeePlayer()
+    {
+        RaycastHit2D[] hits = Physics2D.LinecastAll(transform.position, player.transform.position);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            GameObject other = hit.collider.gameObject;
+            if (other.CompareTag("Walls")) return false; // walls break los
+            if (other.CompareTag("Player")) return true; // can see player
+        }
+
+        return false;
+    }
+
+    // handle collisions with walls and player
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // get the other object
+        GameObject other = col.gameObject;
+
+        // if it's a wall, get stunned
+        if (other.CompareTag("Walls"))
+        {
+            Vector3 normal = col.contacts[0].normal;
+            Vector3 vel = rb.velocity;
+            Debug.Log("Velocity: (" + chargeDir.x.ToString() + ", " + chargeDir.y.ToString() + ")");
+            Debug.Log("Angle: " + Vector3.Angle(chargeDir, -normal).ToString());
+            if(Vector3.Angle(vel, -normal) > COLLISION_SLIDE_ANGLE)
+            {
+
+            } else
+            {
+
+                rb.velocity = new Vector2(-chargeDir.x, -chargeDir.y) * REBOUND_SPEED;
+                charging = 4;
+                chargeTimer = Time.time + CHARGE_STUN;
+            }
+        }
+        // if it's a player, stop for a sec
+        if(other.CompareTag("Player"))
+        {
+            rb.velocity = new Vector2(-chargeDir.x, -chargeDir.y) * CHARGE_SPEED;
+            charging = 4;
+            chargeTimer = Time.time + CHARGE_STUN;
         }
     }
 }
