a06c9d82049dec44ebc3153275866b1e4da296ee
Steven Franklin <steve11sjf@gmail.com>
2020-08-20 03:47:45 -0500

    feature (#14): Dialogue system
    - Added a speed variable to the DialogueManager that controls speed
    - speed controls the number of frames between letters printed
    - 0 is instant, 1 is 60 char/s, 3 is 20 char/s, etc

--- Inferno/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Inferno/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public int typeSpeed; // typing machine go brrrrrrrrr
+
     public Text nameText; // the name text-box on the canvas
     public Text dialogueText; // the dialogue text box
 
@@ -50,9 +52,24 @@ public class DialogueManager : MonoBehaviour
 
         // get the next sentence
         string sentence = sentences.Dequeue();
-        
-        // set the dialogue text to sentence
-        dialogueText.text = sentence;
+
+        // type sentence
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence, typeSpeed));
+    }
+
+    // types a sentence one character every (speed) frames
+    IEnumerator TypeSentence (string sentence, int speed)
+    {
+        dialogueText.text = "";
+        int count = 0;
+        foreach (char letter in sentence.ToCharArray())
+        {
+            for (int i = 0; i < speed; ++i)
+                yield return null;
+
+            dialogueText.text += letter;
+        }
     }
 
     // ends conversation, empties text boxes and calls GameLogic.EndDialogue()
