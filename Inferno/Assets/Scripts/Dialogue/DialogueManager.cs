5c80f6c0f4f937c342bdbb05e84d8dd8451d1eee
Steven Franklin <steve11sjf@gmail.com>
2020-09-05 20:48:08 -0500

    feature (#27): Rich Text in Dialogue Boxes
    - Dialogue boxes now have support for Unity's rich text, and some
    support for other dialogue features
    - <b></b> will bold text between the tags
    - <i></i> will italicise text between the tags
    - <color=#rrggbbaa></color> will change color to the RGBa color
    specified
    - <p=1.0> will pause the typewriter for the specified number of seconds

--- Inferno/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Inferno/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Dialogue;
 using System.Runtime.InteropServices;
+using System.Linq;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -19,6 +20,12 @@ public class DialogueManager : MonoBehaviour
 
     private bool inDialogue;
     private bool isTyping;
+
+    public List<string> currentRichTextTags;
+    public string currentString;
+    public string tempString;
+    public bool isParsingTag;
+
     public DialogueGraph dialogue;
     public Chat current;
     
@@ -31,6 +38,8 @@ public class DialogueManager : MonoBehaviour
         decisionChoice1.transform.parent.gameObject.SetActive(false);
         decisionChoice2.transform.parent.gameObject.SetActive(false);
         decisionChoice3.transform.parent.gameObject.SetActive(false);
+
+        currentRichTextTags = new List<string>();
     }
 
     // Update is called once per frame
@@ -40,13 +49,17 @@ public class DialogueManager : MonoBehaviour
         {
             if(Input.GetKeyDown("e"))
             {
-                // TODO: handle e advancing dialogue
-
                 // if still typing, complete sentence
                 if (isTyping)
                 {
                     StopAllCoroutines();
+
                     isTyping = false;
+                    currentRichTextTags.Clear();
+                    currentString = "";
+                    tempString = "";
+                    isParsingTag = false;
+
                     messageText.text = dialogue.current.text;
                     ShowAnswers(); // show answers, if any
                 }
@@ -153,18 +166,157 @@ public class DialogueManager : MonoBehaviour
     {
         isTyping = true;
         messageText.text = "";
+        currentString = "";
 
         foreach (char letter in message.ToCharArray())
         {
-            for (int i = 0; i < speed; ++i)
-                yield return null;
+            if (isParsingTag)
+            {
+                if (letter == '>')
+                {
+                    tempString += letter;
+                    float pauseFrames = ParseTag(tempString);
+                    isParsingTag = false;
 
-            messageText.text += letter;
+                    float unpauseTime = Time.time + pauseFrames;
+                    while(unpauseTime > Time.time)
+                    {
+                        yield return null;
+                    }
+                }
+                else
+                {
+                    tempString += letter;
+                }
+            }
+            else if (letter == '<')
+            {
+                isParsingTag = true;
+                tempString = "<";
+            }
+            else
+            {
+                for (int i = 0; i < speed; ++i)
+                    yield return null;
+
+                currentString += letter;
+            }
+
+            // print current string to text box
+            string temp = currentString;
+            foreach(string s in currentRichTextTags)
+            {
+                temp += s;
+            }
+            messageText.text = temp;
+
         }
 
         ShowAnswers();
 
         isTyping = false;
+    }
+
+    /// <summary>
+    /// Parses a tag given by the DialogueManager
+    /// </summary>
+    /// <param name="tag">The tag read from the string</param>
+    /// <returns>the number of frames to pause after the tag</returns>
+    private float ParseTag(string tag)
+    {
+        float retVal = 0.0f;
+        char[] charArr = tag.ToCharArray();
+
+        switch (charArr[1])
+        {
+            // bold
+            case 'b':
+                currentString += "<b>";
+                currentRichTextTags.Insert(0, "</b>");
+                break;
+
+            // italics
+            case 'i':
+                currentString += "<i>";
+                currentRichTextTags.Insert(0, "</i>");
+                break;
+
+            // pause typing
+            case 'p':
+                float pauseLength;
+                string toParse = tag.Substring(3, tag.Length - 4);
+
+                float.TryParse(toParse, out pauseLength);
+                retVal = pauseLength;
+                break;
+
+            // change color
+            case 'c':
+                currentString += tag;
+                currentRichTextTags.Insert(0, "</color>");
+                break;
+
+            // change speed
+            case 's':
+
+                break;
+
+            // closing tag
+            case '/':
+                switch (charArr[2])
+                {
+                    // bold
+                    case 'b':
+                        if (currentRichTextTags.First().Equals("</b>"))
+                        {
+                            currentString += "</b>";
+                            currentRichTextTags.RemoveAt(0);
+                        }
+                        else
+                        {
+                            Debug.Log("DialogueManager: Expected " + currentRichTextTags.First() + " not </b>");
+                        }
+                        break;
+
+                    // italics
+                    case 'i':
+                        if (currentRichTextTags.First().Equals("</i>"))
+                        {
+                            currentString += "</i>";
+                            currentRichTextTags.RemoveAt(0);
+                        }
+                        else
+                        {
+                            Debug.Log("DialogueManager: Expected " + currentRichTextTags.First() + " not </i>");
+                        }
+                        break;
+
+                    // color
+                    case 'c':
+                        if (currentRichTextTags.First().Equals("</color>"))
+                        {
+                            currentString += "</color>";
+                            currentRichTextTags.RemoveAt(0);
+                        }
+                        else
+                        {
+                            Debug.Log("DialogueManager: Expected " + currentRichTextTags.First() + " not " + tag);
+                        }
+                        break;
+
+                    // unknown
+                    default:
+
+                        break;
+                }
+                break;
+
+            default:
+
+                break;
+        }
+
+        return retVal;
     }
 
     /// <summary>
