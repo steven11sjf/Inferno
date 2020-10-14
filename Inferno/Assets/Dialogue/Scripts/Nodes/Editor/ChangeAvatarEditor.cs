dbdef800f4bea97e753190715c4a348250892362
Steven Franklin <steve11sjf@gmail.com>
2020-10-14 17:43:47 -0500

    feature (#33):
    - Updated the ChangeAvatar node editor to hide the output node and
    enable drag-and-drop sprite changes

---
+++ Inferno/Assets/Dialogue/Scripts/Nodes/Editor/ChangeAvatarEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+using XNodeEditor;
+
+namespace DialogueEditor
+{
+    [CustomNodeEditor(typeof(Dialogue.ChangeAvatar))]
+    public class ChangeAvatarEditor : NodeEditor
+    {
+
+        public override void OnBodyGUI()
+        {
+            serializedObject.Update();
+
+            Dialogue.ChangeAvatar node = target as Dialogue.ChangeAvatar;
+
+            NodeEditorGUILayout.PortField(target.GetInputPort("input"), GUILayout.Width(100));
+            // the input port
+            EditorGUILayout.Space();
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("source"));
+
+            //NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("text"), GUIContent.none);
+            //NodeEditorGUILayout.DynamicPortList("answers", typeof(DialogueBaseNode), serializedObject, NodePort.IO.Output, Node.ConnectionType.Override);
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public override int GetWidth()
+        {
+            return 336;
+        }
+    }
+}
